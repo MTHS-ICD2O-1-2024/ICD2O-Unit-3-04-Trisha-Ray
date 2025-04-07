8f666491853ee4781c2a6d4e2b1f533d5e7d5227
default <trisha.ray@stu.ocsb.ca>
2025-04-07 17:00:43 +0000

    template

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Trisha Ray
+// Created on: April 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function calculateVolumeOfSphere () {
+  // input
+  const radius = parseFloat(document.getElementById('radius').value);
+
+  // process
+  const volumeOfSphere = 4/3 * Math.PI * (radius * radius * radius);
+
+  // output
+  document.getElementById('volume').innerHTML = 'Volume is: ' + volumeOfSphere + ' mm³'
+}
+
